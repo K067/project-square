0fb82a62067fb1a560034e803d6e1363cf5c4425
aismeg <iaskja20@gmail.com>
2022-11-16 17:44:28 +0300

    delta

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,12 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
+    devServer: {
+        hot: true,
+        static: {
+            directory: './dist',
+            watch: true
+        }
+
+    }
 };
