f98b8b7eb68030796f96474157562f35dd1eb1e8
aismeg <iaskja20@gmail.com>
2022-11-16 04:08:04 +0300

    alpha

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import timer from './modules/timer';
-// import two from './modules/two';
+import menu from './modules/menu';
+import modal from './modules/modal';
 
 timer('20 november 2022');
-// two();
+menu();
+modal();
---
+++ src/modules/modal.js
@@ -0,0 +1,37 @@
+const modal = () => {
+    const buttons = document.querySelectorAll('.popup-btn');
+    const modal = document.querySelector('.popup');
+    const closeBtn = modal.querySelector('.popup-close');
+    const content = modal.querySelector('.popup-content');
+    const width = document.documentElement.clientWidth;
+
+    let count = -10;
+    let moveIt;
+
+    const modalAnim = () => {
+        count++;
+        moveIt = requestAnimationFrame(modalAnim);
+
+        if (width < 768) {
+            return false;
+        }
+        if (count < 5) {
+            content.style.top = count * 20 + 'px';
+        } else {
+            cancelAnimationFrame(moveIt);
+        }
+    };
+
+    buttons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modal.style.display = 'block';
+
+            modalAnim();
+        });
+    });
+    closeBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+};
+
+export default modal;
