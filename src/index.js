791c2c2b2f522e2004f3c904f32c62dcf9bd7401
aismeg <iaskja20@gmail.com>
2022-11-19 20:29:09 +0300

    validatron added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
+import valid from './modules/valid';
 
 timer('20 november 2022');
 menu();
 modal();
+valid();
