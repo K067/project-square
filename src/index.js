a2d3ab817b57862857ff70bef5fbd13b171051bf
aismeg <iaskja20@gmail.com>
2022-11-22 18:10:42 +0300

    slider

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from './modules/menu';
 import modal from './modules/modal';
 import valid from './modules/valid';
 import tabs from './modules/tabs';
+import slider from './modules/slider';
 
 timer('20 november 2023');
 menu();
 modal();
 valid();
 tabs();
+slider();
