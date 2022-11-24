4f0fc95eb94275eee8bd19c77ee13b91955f2d12
aismeg <iaskja20@gmail.com>
2022-11-24 18:55:50 +0300

    calc

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -26,7 +26,7 @@ const calc = (price = 100) => {
         }
 
         if (calcType.value && calcSquare.value) {
-            totalValue = price * calcTypeValue * calcSquareValue * calcCountValue * calcDayValue;
+            totalValue = Math.ceil(price * calcTypeValue * calcSquareValue * calcCountValue * calcDayValue);
 
         } else {
             totalValue = 0;
